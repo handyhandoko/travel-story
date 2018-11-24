ee90ed8fe1ce58b3d450d6fca332f1af3f1695ed
handy handoko <phan.handy.handoko@gmail.com>
2018-11-24 18:55:27 +0700

    add preview to new story

--- resources/views/partials/new_story.blade.php
+++ resources/views/partials/new_story.blade.php
@@ -1,10 +1,23 @@
-{!! Form::open(['url' => 'story']) !!}
-    <div class="card">
+<div class="card">
+    <form method="post" action="{{ url('/images-save') }}" enctype="multipart/form-data" class="dropzone" id="my-dropzone">
+        {{ csrf_field() }}
+        <div class="dz-message">
+            <div class="col-xs-8">
+                <div class="message">
+                    <p>Drop files here or Click to Upload</p>
+                </div>
+            </div>
+        </div>
+        <div class="fallback">
+            <input type="file" name="file" multiple>
+        </div>
+    </form>
+    {!! Form::open(['url' => 'story']) !!}
         {{ Form::textarea('content', null, array('placeholder'=>'Ceritakan kisah perjalanan mu disini',
-        'class'=>'autoExpand', 'rows'=>'3', 'data-min-rows'=>'3')) }}
+    'class'=>'autoExpand', 'rows'=>'3', 'data-min-rows'=>'3')) }}
 
         <div class="justify-content-end">
             {!! Form::submit('Post kisah mu!', array('class'=>'btn btn-primary post-story float-right')) !!}
-        </div>    
-    </div>
-{!! Form::close() !!}
+        </div>
+    {!! Form::close() !!}
+</div>
