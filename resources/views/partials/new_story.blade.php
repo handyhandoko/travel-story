8c217b275db3fc6cc5b67209a7b70b95040fe514
handy handoko <phan.handy.handoko@gmail.com>
2018-11-24 14:18:11 +0700

    ganti textarea menjadi laravel collective

--- resources/views/partials/new_story.blade.php
+++ resources/views/partials/new_story.blade.php
@@ -1,3 +1,8 @@
-<div class="card">
-    <textarea class='autoExpand' rows='3' data-min-rows='3' placeholder='Auto-Expanding Textarea'></textarea>
-</div>
+{!! Form::open(['url' => 'foo/bar']) !!}
+    <div class="card">
+        {{ Form::textarea('content', null, array('placeholder'=>'Ceritakan kisah perjalanan mu disini',
+        'class'=>'autoExpand', 'rows'=>'3', 'data-min-rows'=>'3')) }}
+    </div>
+    
+    {!! Form::submit('Post kisah mu!') !!}
+{!! Form::close() !!}
