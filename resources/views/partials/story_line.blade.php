448050756bee35a62509b7cc976b03f844034673
handy handoko <phan.handy.handoko@gmail.com>
2018-11-24 15:50:41 +0700

    add read more to story line

--- resources/views/partials/story_line.blade.php
+++ resources/views/partials/story_line.blade.php
@@ -4,6 +4,9 @@
 
 @foreach($stories as $story)
     <div class="card story">
-            {{ $story->content }}
+        {{ str_limit(strip_tags($story->content), 500) }}
+        @if (strlen(strip_tags($story->content)) > 50)
+            ... <a href="{{URL::to('story/' . $story->id) }}">Read More</a>
+        @endif
     </div>
 @endforeach
