c0e924d9d2408097cfa06b4d428dc2b344e2b700
handy handoko <phan.handy.handoko@gmail.com>
2018-11-24 13:56:29 +0700

    add expandable text area

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,19 @@
 @extends('layouts.app')
 
+@push('css_style')
+    <link href="css/expandable_textarea.css" rel="stylesheet" >
+@endpush
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+            @include('partials/new_story')
         </div>
     </div>
 </div>
 @endsection
+
+@push('js_script')
+    <script type="text/javascript" src="js/expandable_textarea.js"></script>
+@endpush
---
+++ resources/views/partials/new_story.blade.php
@@ -0,0 +1,3 @@
+<div class="card">
+    <textarea class='autoExpand' rows='3' data-min-rows='3' placeholder='Auto-Expanding Textarea'></textarea>
+</div>
