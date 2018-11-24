501d6dfebf7885c8729f2783b47a5a15d5f7cb76
handy handoko <phan.handy.handoko@gmail.com>
2018-11-24 15:48:05 +0700

    show full story page

---
+++ resources/views/pages/full_story.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@push('css_style')
+    <link href="css/style.css" rel="stylesheet" >
+@endpush
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            @foreach (explode("\n", $story->content) as $paragraph)
+                @if(trim($paragraph))
+                    <p>{{$paragraph}}</p>
+                @endif
+            @endforeach
+        </div>
+    </div>
+</div>
+@endsection
