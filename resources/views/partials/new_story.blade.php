ddabfe5ced3945abb6695c3889082838606536fe
handy handoko <phan.handy.handoko@gmail.com>
2018-11-24 15:06:00 +0700

    tidy up submit button

--- resources/views/partials/new_story.blade.php
+++ resources/views/partials/new_story.blade.php
@@ -2,7 +2,9 @@
     <div class="card">
         {{ Form::textarea('content', null, array('placeholder'=>'Ceritakan kisah perjalanan mu disini',
         'class'=>'autoExpand', 'rows'=>'3', 'data-min-rows'=>'3')) }}
+
+        <div class="justify-content-end">
+            {!! Form::submit('Post kisah mu!', array('class'=>'btn btn-primary post-story float-right')) !!}
+        </div>    
     </div>
-    
-    {!! Form::submit('Post kisah mu!', array('class'=>'btn btn-primary')) !!}
 {!! Form::close() !!}
