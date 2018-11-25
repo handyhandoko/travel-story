7a923f9b5bb03bf3c8fe36abe4f00fa5c5cb3082
handy handoko <phan.handy.handoko@gmail.com>
2018-11-25 09:26:18 +0700

    add whatsapp to trijek

--- resources/views/pages/trijek.blade.php
+++ resources/views/pages/trijek.blade.php
@@ -24,70 +24,70 @@
                 Trijek area:<br/>
                 TRIJEK LOMBOK :<br/>
                 <ol>
-                    <li>bang fajri : 085333412180</li>
-                    <li>bang rizky : 085338572472</li>
-                    <li>bang lalu zull : 087865118168</li>
-                    <li>bang syarief : 081907582664</li>
+                    <li>bang fajri : 085333412180 <a href="https://wa.me/6285333412180">Hubungi via Whatsapp<a></li>
+                    <li>bang rizky : 085338572472 <a href="https://wa.me/6285338572472">Hubungi via Whatsapp<a></li>
+                    <li>bang lalu zull : 087865118168 <a href="https://wa.me/6287865118168">Hubungi via Whatsapp<a></li>
+                    <li>bang syarief : 081907582664 <a href="https://wa.me/6281907582664">Hubungi via Whatsapp<a></li>
                 </ol>
             
                 TRIJEK SUMBAWA :
                 <ol>
-                    <li>bang fajri : 085333412180</li>
-                    <li>bang arif : 082341296696</li>
+                    <li>bang fajri : 085333412180 <a href="https://wa.me/6285333412180">Hubungi via Whatsapp<a></li>
+                    <li>bang arif : 082341296696 <a href="https://wa.me/6282341296696">Hubungi via Whatsapp<a></li>
                 </ol>
             
                 TRIJEK BALI / NUSPEN :
                 <ol>
-                    <li>mbo kusuma : 081238499189</li>
+                    <li>mbo kusuma : 081238499189 <a href="https://wa.me/6281238499189">Hubungi via Whatsapp<a></li>
                 </ol>
             
                 TRIJEK MALANG :
                 <ol>
-                    <li>bang Eka : 081349728484</li>
+                    <li>bang Eka : 081349728484 <a href="https://wa.me/6281349728484">Hubungi via Whatsapp<a></li>
                 </ol>
             
                 TRIJEK JOGJA :
                 <ol>
-                    <li>bang risman : 081578510677</li>
-                    <li>bang dadang : 082138875919</li>
-                    <li>bang asep : 082243428210</li>
-                    <li>mba tri sumarni : 08991895954</li>
+                    <li>bang risman : 081578510677 <a href="https://wa.me/6281578510677">Hubungi via Whatsapp<a></li>
+                    <li>bang dadang : 082138875919 <a href="https://wa.me/6282138875919">Hubungi via Whatsapp<a></li>
+                    <li>bang asep : 082243428210 <a href="https://wa.me/6282243428210">Hubungi via Whatsapp<a></li>
+                    <li>mba tri sumarni : 08991895954 <a href="https://wa.me/628991895954">Hubungi via Whatsapp<a></li>
                 </ol>
             
                 TRIJEK CIREBON :
                 <ol>
-                    <li>bang irawan : 08996311241</li>
-                    <li>bang sukron : 087728799888</li>
+                    <li>bang irawan : 08996311241 <a href="https://wa.me/628996311241">Hubungi via Whatsapp<a></li>
+                    <li>bang sukron : 087728799888 <a href="https://wa.me/6287728799888">Hubungi via Whatsapp<a></li>
                 </ol>
             
                 TRIJEK BOGOR :
                 <ol>
-                    <li>teh noey : 08811737104</li>
+                    <li>teh noey : 08811737104 <a href="https://wa.me/628811737104">Hubungi via Whatsapp<a></li>
                 </ol>
             
                 TRIJEK DIENG :
                 <ol>
-                    <li>bang ananda : 082221515684</li>
+                    <li>bang ananda : 082221515684 <a href="https://wa.me/6282221515684">Hubungi via Whatsapp<a></li>
                 </ol>
             
                 TRIJEK PACITAN :
                 <ol>
-                    <li>bang momo : 082221368808</li>
+                    <li>bang momo : 082221368808 <a href="https://wa.me/6282221368808">Hubungi via Whatsapp<a></li>
                 </ol>
             
                 TRIJEK LAMPUNG :
                 <ol>
-                    <li>bang hafiz : 085558805599</li>
+                    <li>bang hafiz : 085558805599 <a href="https://wa.me/6285558805599">Hubungi via Whatsapp<a></li>
                 </ol>
             
                 TRIJEK BAUBAU BUTON :
                 <ol>
-                    <li>bang monde : 082259502119</li>
+                    <li>bang monde : 082259502119 <a href="https://wa.me/6282259502119">Hubungi via Whatsapp<a></li>
                 </ol>
             
                 TRIJEK BANYUWANGI :
                 <ol>
-                    <li>bang franky : 081357054740</li>
+                    <li>bang franky : 081357054740 <a href="https://wa.me/6281357054740">Hubungi via Whatsapp<a></li>
                 </ol>
             </p>
         </div>
